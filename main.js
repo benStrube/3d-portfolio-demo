5b8a3f69a349f51fb00d76e3636c6c3ff0f869c8
bstrubeXpanxion <bstrube@xpanxion.com>
2022-01-27 23:15:31 -0500

    added random star generation function

--- main.js
+++ main.js
@@ -49,6 +49,23 @@ scene.add(lightHelper, gridHelper);
 //orbit controls
 const controls = new OrbitControls(camera, renderer.domElement);
 
+//randomly generate stars
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
+  scene.add(star);
+}
+
+//call star generate function
+Array(200).fill().forEach(addStar);
+
 //scene refreshing loop
 function animate() {
   requestAnimationFrame(animate);
